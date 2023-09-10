ed263f3ed2ecba16e7604c9c923843ca9a9ecf44
rupesh-1010 <rupeshnepali30@gmail.com>
2023-09-10 18:05:21 +0545

    js started

---
+++ figma-design.html/index.js
@@ -0,0 +1,55 @@
+console.log("external-js-connected")
+
+
+/* (alt + tab key le switch garne sakinxa full screen ma chakirako xa vane )semi colon nadida pani kei hudaina js le aafai le auto rakhdinxa */
+
+// hamro coding ko principle DRY
+// "alt+tab" swotch betn tabs
+// ctrl + / line comment
+// ctrl+shift+a multi line comment
+
+//    variables
+//     -some keywords which can hold values
+//     -meant to be re-used
+
+
+greeting="Hello"
+// console.log("Namaste ram");
+// console.log("Namaste shyam");
+// console.log("Namaste nyauri");
+// console.log("Namaste ram");
+// console.log("Namaste shyam");
+// console.log("Namaste nyauri");
+
+console.log(greeting, " ram");
+console.log(greeting, " shyam");
+console.log(greeting, " nyauri");
+console.log(greeting, " ram");
+console.log(greeting, " shyam");
+console.log(greeting, " nyauri");
+
+firstName = "Hari"
+lastName = "Bahadur"
+// string concatenation
+fullName = firstName + lastName
+full_Name = firstName + lastName
+age=20
+paidStatus = true
+
+
+
+
+console.log("fullName is",fullName)
+console.log("full_Name is",full_Name)
+console.log(20)
+console.log(age)
+console.log("age is", age)
+console.log("paidStatus is",paidStatus)
+
+// data types 
+//  - text - number 
+//  - number - Number
+
+//  boolean = true / false 
+
+
